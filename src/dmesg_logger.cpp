0897113b9803ea35b10323889b96007eb41570b8
gregorthebigmac <gregorthebigmac@gmail.com>
2019-10-18 05:13:01 -0500

    got it working with a diff function to avoid unecessary logging!

--- src/dmesg_logger.cpp
+++ src/dmesg_logger.cpp
@@ -15,17 +15,21 @@ using std::endl;
 using std::string;
 using std::vector;
 
-vector<string> terminal_feedback, error_list;
+vector<string> dmesg_1, dmesg_2, dmesg_out, error_list;
 
 command cmd;
+
+void diff_dmesg();
 
 int main() {
 	// This will run 300 times, with a 1 sec delay, for a grand total of 5 min
 	// before quitting. This will result in 300 sequentially numbered text
 	// files ranging from 001-300.
 	for (int i = 1; i < 301; i++) {
-		terminal_feedback.clear();
+		dmesg_1 = dmesg_2;
+		dmesg_2.clear();
 		error_list.clear();
+		dmesg_out.clear();
 		string log_num = num_to_str(i);
 		
 		if (i < 10)
@@ -34,7 +38,7 @@ int main() {
 			log_num = "0" + log_num;
 		
 		string log_filename = "dmesg_log_" + log_num + ".txt";
-		cmd.exec("dmesg", terminal_feedback, error_list);
+		cmd.exec("dmesg", dmesg_2, error_list);
 		if (error_list.size() > 0) {
 			cout << "ERROR: " << endl;
 			for (int j = 0; j < error_list.size(); j++) {
@@ -43,12 +47,14 @@ int main() {
 			return 1;
 		}
 		
+		diff_dmesg();
+		
 		std::ofstream fout;
 		log_filename = "data/" + log_filename;
 		fout.open(log_filename.c_str());
 		if (fout.is_open()) {
-			for (int x = 0; x < terminal_feedback.size(); x++) {
-				fout << terminal_feedback[x];
+			for (int x = 0; x < dmesg_out.size(); x++) {
+				fout << dmesg_out[x];
 			}
 			fout.close();
 		}
@@ -59,4 +65,34 @@ int main() {
 		usleep(1000 * 1000);	// in microseconds. 1000 * 1000 = 1 second
 	}
 	return 0;
+}
+
+void diff_dmesg() {
+	int loop_length = 0;
+	cout << "loop_length = [" << loop_length << "]" << endl;
+	cout << "dmesg_1.size() = [" << dmesg_1.size() << "]" << endl;
+	cout << "dmesg_2.size() = [" << dmesg_2.size() << "]" << endl;
+	if (dmesg_1.size() > dmesg_2.size()) {
+		loop_length = dmesg_2.size();
+		cout << "dmesg_2 is smaller, loop_length is now = dmesg_2.size()" << endl;
+	}
+	else {
+		loop_length = dmesg_1.size();
+		cout << "dmesg_1 is smaller, loop_length is now = dmesg_1.size()" << endl;
+	}	
+	for (int i = 0; i < loop_length; i++) {
+		cout << "dmesg_1[" << i << "] = [" << dmesg_1[i] << "]" << endl;
+		cout << "dmesg_2[" << i << "] = [" << dmesg_2[i] << "]" << endl;
+		if (dmesg_1[i] != dmesg_2[i]) {
+			dmesg_out.push_back(dmesg_2[i]);
+			cout << "dmesg_1[" << i << "] != dmesg_2[" << i << "]" << endl;
+			cout << "pushing dmesg_2[" << i << "] to dmesg_out" << endl;
+		}
+	}
+	if (dmesg_2.size() > loop_length) {
+		cout << "There are leftover lines in dmesg_2. Pushing them back to dmesg_out" << endl;
+		for (int i = loop_length; i < dmesg_2.size(); i++) {
+			dmesg_out.push_back(dmesg_2[i]);
+		}
+	}
 }
